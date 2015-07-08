2a0bdd8538b94b8e394d5b21f347dd60faf74bcd
Christy Haragan <christy.haragan@gmail.com>
2015-07-08 09:02:44 +0100

    Update to latest version of uservices

--- lib/localProxy.ts
+++ lib/localProxy.ts
@@ -1,34 +1,38 @@
-import {Spec, Observable, splitService} from 'uservices'
-import {Observer} from 'rx'
+import {visitSpec, Spec} from 'uservices'
+import {Observer, Observable} from 'rx'
+import * as s from 'typescript-schema'
 
-export function createLocalProxy<T>(server: SocketIO.Server, spec: Spec<T>, service: T) {
-  let split = splitService(spec, service)
-
+export function createLocalProxy<T>(server: SocketIO.Server, serviceSchema: Spec, service: T) {
   server.on('connection', function(socket) {
-    split.visit(function(name, action) {
-      socket.on(name, action)
-    }, function(name, func) {
-      socket.on(name, function(args: any[], cb: (value?: any, error?: any) => void) {
-        (<Promise<any>>func.apply(service, args)).then(cb).catch(cb.bind(null, null))
-      })
-    }, function(name, event) {
-      socket.on(name, function(args: any[], cb: (id: string) => void) {
-        let id = String(Date.now())
-        socket.once(name + id, function() {
-          (<Observable<any>>event.apply(service, args)).subscribe({
-            onNext: function(value) {
-              socket.emit(name + id, [value])
-            },
-            onError: function(error) {
-              socket.emit(name + id, [null, error])
-            },
-            onCompleted: function() {
-              socket.emit(name + id)
-            }
-          })
+    visitSpec({
+      onPromise: function(memberSchema){
+        let name = memberSchema.name
+        let func = service[name]
+        socket.on(name, function(args: any[], cb: (value?: any, error?: any) => void) {
+          (<Promise<any>>func.apply(service, args)).then(cb).catch(cb.bind(null, null))
         })
-        cb(id)
-      })
-    })
+      },
+      onObservable: function(memberSchema){
+        let name = memberSchema.name
+        let event = service[name]
+        socket.on(name, function(args: any[], cb: (id: string) => void) {
+          let id = String(Date.now())
+          socket.once(name + id, function() {
+            (<Observable<any>>event.apply(service, args)).subscribe(
+              function(value) {
+                socket.emit(name + id, [value])
+              },
+              function(error) {
+                socket.emit(name + id, [null, error])
+              },
+              function() {
+                socket.emit(name + id)
+              }
+            )
+          })
+          cb(id)
+        })
+      }
+    }, /*Type Hack*/ <s.Class> serviceSchema)
   })
 }
